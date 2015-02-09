3e1d4372de89cc9b5a0fa85a412ba0d0266661e8
seyedrezamirkhani <seyedrezamirkhani@yahoo.co.uk>
2015-02-09 09:25:42 +0000

    imporoved code for reading input file

--- plot3.R
+++ plot3.R
@@ -11,11 +11,14 @@ if (!"data.table" %in% installed.packages()) install.packages(data.table)
 library(data.table)
 
 # read data for 1st and 2nd of Feb 2007
-dt_data <- fread("household_power_consumption.txt", header = FALSE, sep = ";",
-                 na.strings = "?", colClasses=c("character", "character", "numeric",
-                                                "numeric", "numeric", "numeric", 
-                                                "numeric", "numeric", "numeric"),
-                 skip=66637, nrows = 2880)
+
+# Convert the dates needed for subsetting into R Date/Time class
+dates_to_read <- difftime(as.POSIXct("2007-02-03"), as.POSIXct("2007-02-01"), units = "mins")
+rows_to_read <- as.numeric(dates_to_read)
+
+# Read the data for 1st and 2nd of Feb 2007 
+dt_data <- fread(input = "household_power_consumption.txt", sep = ";", 
+                 skip = "1/2/2007", nrows = rows_to_read, na.strings = c("?", ""))
 
 # read header names only
 dt_header <- fread("household_power_consumption.txt", header = TRUE, sep = ";", nrows = 0)
